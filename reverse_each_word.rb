8fd88b59654ac9d4da4719e1687dfa8d42188dcd
Jeremy Garnes <j06garnes@gmail.com>
2020-01-13 19:51:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,8 @@ def reverse_each_word(sentence1)
   # First convert string to Array
   # Then iterate over array using each index
   # shovel each index into new array in reverse order 
- new_array = []
  new_sentence = sentence1.split(" ")
- new_sentence.each do |w| w.reverse 
-end 
- reverse_each_word("Hello there, and how are you?")
+  new_sentence.map { |w| w.reverse} 
 end
+reverse_each_word()
 
